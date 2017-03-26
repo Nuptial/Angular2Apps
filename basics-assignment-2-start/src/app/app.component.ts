50a3891132908294e178d9c8c1ac26f31c192d1b
Alican Camlibel <alican.camlibel91@gmail.com>
2017-03-26 20:18:41 +0300

    first commit

---
+++ basics-assignment-2-start/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  username:string = null;
+
+  checkUsername(){
+    if(this.username != ' ')
+      return true;
+    else{
+    return false;
+    }
+  }
+
+  resetUsername(){
+    this.username = null;
+  }
+}
